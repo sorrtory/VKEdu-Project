8d9c64f984ad0c22b60257f52c9008c4df30112a
antiqueMouse <s.yakovlev06@yahoo.com>
2026-03-30 16:31:04 +0300

    first proto conference

---
+++ frontend/src/components/conference.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import {
+  LiveKitRoom,
+  VideoConference,
+  ControlBar,
+  RoomAudioRenderer,
+} from '@livekit/components-react';
+import '@livekit/components-styles';
+
+interface ConferenceRoomProps {
+  roomName: string;
+  userName: string;
+  userId: string;
+  serverUrl: string;
+  token: string;
+}
+
+export default function ConferenceRoom({
+  roomName,
+  userName,
+  userId,
+  serverUrl,
+  token,
+}: ConferenceRoomProps) {
+  return (
+    <div className="conference-wrapper" style={{ height: '100vh', width: '100vw' }}>
+      <LiveKitRoom
+        serverUrl={serverUrl}
+        token={token}
+        connect={true}
+        audio={true}
+        video={true}
+        data-lk-theme="default"
+      >
+        <VideoConference />
+        <ControlBar variation="minimal" />
+        <RoomAudioRenderer />
+      </LiveKitRoom>
+    </div>
+  );
+}
