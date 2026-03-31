6abf4e284cb59fb44f51d9645d2dea6ca3ac55c1
antiqueMouse <s.yakovlev06@yahoo.com>
2026-03-31 07:29:56 +0300

    small gui fix

--- frontend/src/components/conference.tsx
+++ frontend/src/components/conference.tsx
@@ -35,7 +35,6 @@ export default function ConferenceRoom({
         data-lk-theme="default"
       >
         <VideoConference />
-        <ControlBar variation="minimal" />
         <RoomAudioRenderer />
       </LiveKitRoom>
     </div>
