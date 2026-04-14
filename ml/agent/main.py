7bd5cf0bad6691eab161ef3932d7102f57131c56
Alexei Noskov <alexnoskov_44@mail.ru>
2026-04-14 16:20:08 +0300

    agent room-joining realisation

---
+++ ml/agent/main.py
@@ -0,0 +1,22 @@
+import asyncio
+import logging
+from livekit.agents import AutoSubscribe, JobContext, WorkerOptions, cli, WorkerType
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("livekit-agent")
+
+async def entrypoint(ctx: JobContext):
+    logger.info(f"Joining room {ctx.room.name}...")
+
+    await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
+
+    logger.info(f"Joined room {ctx.room.name}.")
+
+    await asyncio.Event().wait()
+
+if __name__ == "__main__":
+    cli.run_app(WorkerOptions(
+        entrypoint_fnc=entrypoint,
+        worker_type=WorkerType.ROOM,
+        agent_name="default-agent",
+    ))
